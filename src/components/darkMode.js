328cbe102f427b5276713d752c185f0c8d88bc64
Veronica Romero Cores <veromerocores@gmail.com>
2024-05-14 22:56:17 +0100

    todays version

--- src/components/darkMode.js
+++ src/components/darkMode.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const DarkModes = ({ theme, toggleTheme }) => {
+    
   return (
     <label className="switch">
       <input type="checkbox" checked={theme === 'dark'} onChange={toggleTheme} />
